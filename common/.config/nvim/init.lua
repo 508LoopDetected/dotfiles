e807e35f3f421292de626b9152083011c5435a00
Michael Milstead <themilstead@gmail.com>
2026-03-11 09:39:48 -0500

    telescope updates

--- common/.config/nvim/init.lua
+++ common/.config/nvim/init.lua
@@ -329,6 +329,7 @@ require("lazy").setup({
 				{ "<leader>s", group = "[S]earch", mode = { "n", "v" } },
 				{ "<leader>t", group = "[T]oggle" },
 				{ "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
+				{ "gr", group = "LSP [G]oto/[R]efactor" },
 			},
 		},
 	},
@@ -435,50 +436,46 @@ require("lazy").setup({
 				callback = function(event)
 					local buf = event.buf
 
+					-- Use dropdown theme for LSP pickers so they appear as centered popups
+					-- instead of full-screen Telescope windows (similar to Sublime Text's LSP panels)
+					local dropdown = require("telescope.themes").get_dropdown({ winblend = 10 })
+
 					-- Find references for the word under your cursor.
-					vim.keymap.set("n", "grr", builtin.lsp_references, { buffer = buf, desc = "[G]oto [R]eferences" })
+					vim.keymap.set("n", "grr", function()
+						builtin.lsp_references(dropdown)
+					end, { buffer = buf, desc = "[G]oto [R]eferences" })
 
 					-- Jump to the implementation of the word under your cursor.
 					-- Useful when your language has ways of declaring types without an actual implementation.
-					vim.keymap.set(
-						"n",
-						"gri",
-						builtin.lsp_implementations,
-						{ buffer = buf, desc = "[G]oto [I]mplementation" }
-					)
+					vim.keymap.set("n", "gri", function()
+						builtin.lsp_implementations(dropdown)
+					end, { buffer = buf, desc = "[G]oto [I]mplementation" })
 
 					-- Jump to the definition of the word under your cursor.
 					-- This is where a variable was first declared, or where a function is defined, etc.
 					-- To jump back, press <C-t>.
-					vim.keymap.set("n", "grd", builtin.lsp_definitions, { buffer = buf, desc = "[G]oto [D]efinition" })
+					vim.keymap.set("n", "grd", function()
+						builtin.lsp_definitions(dropdown)
+					end, { buffer = buf, desc = "[G]oto [D]efinition" })
 
 					-- Fuzzy find all the symbols in your current document.
 					-- Symbols are things like variables, functions, types, etc.
-					vim.keymap.set(
-						"n",
-						"gO",
-						builtin.lsp_document_symbols,
-						{ buffer = buf, desc = "Open Document Symbols" }
-					)
+					vim.keymap.set("n", "gO", function()
+						builtin.lsp_document_symbols(dropdown)
+					end, { buffer = buf, desc = "Open Document Symbols" })
 
 					-- Fuzzy find all the symbols in your current workspace.
 					-- Similar to document symbols, except searches over your entire project.
-					vim.keymap.set(
-						"n",
-						"gW",
-						builtin.lsp_dynamic_workspace_symbols,
-						{ buffer = buf, desc = "Open Workspace Symbols" }
-					)
+					vim.keymap.set("n", "gW", function()
+						builtin.lsp_dynamic_workspace_symbols(dropdown)
+					end, { buffer = buf, desc = "Open Workspace Symbols" })
 
 					-- Jump to the type of the word under your cursor.
 					-- Useful when you're not sure what type a variable is and you want to see
 					-- the definition of its *type*, not where it was *defined*.
-					vim.keymap.set(
-						"n",
-						"grt",
-						builtin.lsp_type_definitions,
-						{ buffer = buf, desc = "[G]oto [T]ype Definition" }
-					)
+					vim.keymap.set("n", "grt", function()
+						builtin.lsp_type_definitions(dropdown)
+					end, { buffer = buf, desc = "[G]oto [T]ype Definition" })
 				end,
 			})
 
@@ -748,6 +745,19 @@ require("lazy").setup({
 				--
 				-- You can use 'stop_after_first' to run the first available formatter from the list
 				-- javascript = { "prettierd", "prettier", stop_after_first = true },
+			},
+			-- MM edit: Run prettier from the nearest dir containing a prettierrc,
+			-- so it finds plugins (e.g. prettier-plugin-svelte) in monorepo subdirs
+			formatters = {
+				prettier = {
+					cwd = function(self, ctx)
+						return require("conform.util").root_file({
+							".prettierrc",
+							".prettierrc.json",
+							"prettier.config.js",
+						})(self, ctx)
+					end,
+				},
 			},
 		},
 	},
